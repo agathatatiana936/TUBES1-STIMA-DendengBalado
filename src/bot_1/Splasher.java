5fd556aea99d3bdd604c19cb9c645a52b0ebfd29
Jerannn24 <juniornatra72@gmail.com>
2026-03-12 20:50:52 +0700

    feat: refine Splasher behavior for different game rounds and enhance enemy targeting strategies

--- src/bot_1/Splasher.java
+++ src/bot_1/Splasher.java
@@ -3,11 +3,12 @@ package bot_1;
 import battlecode.common.*;
 
 public class Splasher {
+    static MapLocation knownEnemyTower = null;
     public static void runSplasher(RobotController rc) throws GameActionException {
         int round = rc.getRoundNum();
-        if (round <= 200) {
+        if (round <= 600) {
             runEarly(rc);
-        } else if (round <= 800) {
+        } else if (round <= 1000) {
             runMid(rc);
         } else {
             runLate(rc);
@@ -15,29 +16,29 @@ public class Splasher {
     }
 
     private static void runEarly(RobotController rc) throws GameActionException {
-        Direction dir = Motion.moveTowardCenter(rc);
+        Direction dir = Motion.moveTowardNearestEnemyTower(rc);
+        if (dir == null) {
+            dir = Motion.moveTowardCenter(rc);
+        }
         if (dir != null && rc.canMove(dir)) rc.move(dir);
         if (rc.canAttack(rc.getLocation())) rc.attack(rc.getLocation());
     }
 
     private static void runMid(RobotController rc) throws GameActionException {
-        // if (Soldier.tryAttackEnemyTower(rc)) return;
-        // Soldier.tryAttackEnemyTower(rc);
-        // for (RobotInfo enemy : rc.senseNearbyRobots(-1, rc.getTeam().opponent())) {
-        //     if (rc.canAttack(enemy.getLocation())) {
-        //         rc.attack(enemy.getLocation());
-        //         return;
-        //     }
-        // }
-        // Direction dir = Motion.moveTowardEnemyPaint(rc);
-        // if (dir != null && rc.canMove(dir)) rc.move(dir);
-        // if (rc.canAttack(rc.getLocation())) rc.attack(rc.getLocation());
-        Soldier.runEarly(rc);
+        Direction dir = Motion.moveTowardNearestEnemyTower(rc);
+        if (dir == null) dir = Motion.moveTowardCorner(rc);
+        if (dir != null && rc.canMove(dir)) { rc.move(dir); }
+        if (Soldier.tryAttackEnemyTower(rc)) return;
+        Soldier.tryAttackEnemyTower(rc);
+        Soldier.paintCurrentTileIfNeeded(rc);
     }
 
     private static void runLate(RobotController rc) throws GameActionException {
-        Direction dir = Motion.moveTowardCorner(rc);
-        if (dir == null) return;
+        Direction dir = Motion.moveTowardNearestEnemyTower(rc);
+        if (Soldier.tryAttackEnemyTower(rc)) return;
+        if (dir == null) {
+            dir = Motion.moveTowardCorner(rc);
+        }
         MapInfo dirInfo = rc.senseMapInfo(rc.getLocation().add(dir));
         if (rc.canMove(dir)) rc.move(dir);
         if (dirInfo.getPaint() == PaintType.ENEMY_PRIMARY
